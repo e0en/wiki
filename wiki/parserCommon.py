0c0edc745e728121632bfce6c61f1f5ac80ee28d
Yoonseop Kang <e0engoon@gmail.com>
2016-02-12 16:17:35 +0900

    Add option to render in markdown, instead of HTML

--- wiki/parserCommon.py
+++ wiki/parserCommon.py
@@ -22,6 +22,24 @@ def parse_inline(buff, acc):
     return result
 
 
+def parse_inline_markdown(buff, acc):
+    # load inline parsing plug-ins one by one.
+    idx = 0
+    n = len(buff)
+    result = ''
+    while idx < n:
+        offset = 1
+        s = buff[idx]
+        for p in plugins:
+            if p.test(buff, idx):
+                s, offset = p.parse_markdown(buff, idx, acc)
+                break
+        result += s
+        idx += offset
+
+    return result
+
+
 def objParseInline(buff, page):
     idx = 0
     n = len(buff)
--- wiki/wikiParser.py
+++ wiki/wikiParser.py
@@ -1,4 +1,4 @@
-from parserCommon import parse_inline
+from parserCommon import parse_inline, parse_inline_markdown
 import wikiLinePlugins
 import wikiPostProcessors
 import cgi
@@ -76,3 +76,64 @@ class Parser:
         xhtml = str_front + xhtml + str_end
 
         return xhtml
+
+    def parse_markdown(self, raw_text):
+        raw_text = raw_text.replace('\r', '')
+        raw_text = cgi.escape(raw_text)
+
+        lines = raw_text.split('\n')
+
+        parsed = []
+        settings = {}
+        p_buffer = ''
+
+        i = 0
+
+        # process #1. create xhtmls from text
+        while 1:
+            if i >= len(lines):
+                break
+            if lines[i].strip() == '':
+                # make a paragraph
+                if p_buffer.strip() != '':
+                    md = parse_inline_markdown(p_buffer.strip(), self.acc)
+                    parsed.append(md + '\n\n')
+                    p_buffer = ''
+                i += 1
+            else:
+                did_match = False
+                for p in self.line_plugins:
+                    if p.test(lines, i):
+                        if p_buffer.strip() != '':
+                            md = parse_inline_markdown(
+                                    p_buffer.strip(), self.acc)
+                            parsed.append(md + '\n\n')
+                            p_buffer = ''
+                        text, offset = p.parse_markdown(lines, i, self.acc)
+                        did_match = True
+                        parsed.append(text)
+                        i += offset
+                        break
+
+                if not did_match:
+                    # accumulate current line for a paragraph
+                    p_buffer += lines[i] + '\n'
+                    i += 1
+        # flush p_buffer if its content exists
+        if p_buffer.strip() != '':
+            md = parse_inline_markdown(p_buffer.strip(), self.acc)
+            parsed.append(md + '\n\n')
+
+        xhtml = ''
+        for l in parsed:
+            xhtml += l
+        # post_processing step. it makes a list of backlink and stuffs.
+        str_front = ''
+        str_end = ''
+        for f in self.pp_front:
+            str_front += f(self.acc)
+        for f in self.pp_end:
+            str_end += f(self.acc)
+        xhtml = str_front + xhtml + str_end
+
+        return "<pre>%s</pre>" % xhtml.strip()
