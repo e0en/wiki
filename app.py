11ca04af34a4b2056bd306ad28bca13b9a6eae4a
Yoonseop Kang <e0engoon@gmail.com>
2016-02-21 20:11:26 +0900

    Convert most of templates to jinja2 format

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import sqlite3
+from datetime import datetime, date
 from flask import Flask, redirect, url_for, g, render_template
 
 
@@ -38,8 +39,8 @@ def read(pagename):
     res = query_db(query_str, one=True)
 
     if res is not None:
-        return render_template("Read.html")
-        return "<pre>%s</pre>" % res["markdown"]
+        print res.keys()
+        return render_template("Read.html", article=res)
     else:
         return redirect(url_for('search', q=pagename))
 
@@ -47,17 +48,61 @@ def read(pagename):
 @app.route("/edit/<pagename>")
 def edit(pagename):
     query_str = "SELECT * FROM wiki_article WHERE name='%s'" % pagename
-    old = query_db(query_str, one=True)
+    article = query_db(query_str, one=True)
 
-    if old is not None:
-        return "edit %s" % pagename
+    if article is None:
+        article = { "name": pagename }
+    return render_template("Edit.html", article=article)
+
+
+@app.route("/recentchanges")
+def recentchanges():
+    return render_template("RecentChanges.html")
+
+
+@app.route("/pagelist")
+def pagelist():
+    query_str = "SELECT name FROM wiki_article ORDER BY name"
+    article_list = query_db(query_str)
+    return render_template("PageList.html", article_list=article_list)
+
+
+@app.route("/upload")
+def upload():
+    return render_template("Upload.html")
+
+
+@app.route("/search")
+def search():
+    return render_template("Search.html")
+
+
+@app.route("/history_list/<pagename>")
+def history_list(pagename):
+    return "history_list"
+
+
+@app.route("/delete/<pagename>")
+def delete(pagename):
+    return "delete"
+
+
+@app.route("/raw/<pagename>")
+def raw(pagename):
+    query_str = "SELECT * FROM wiki_article WHERE name='%s'" % pagename
+    res = query_db(query_str, one=True)
+
+    if res is not None:
+        return render_template("Read.html")
+        return res["markdown"]
     else:
-        return "create %s" % pagename
+        return redirect(url_for('search', q=pagename))
 
 
-@app.route("/search/<q>")
-def search(q):
-    return "search %s" % q
+@app.route("/jrnl")
+def jrnl():
+    date_str = str(date.today())
+    return redirect(url_for("edit", pagename=date_str))
 
 
 @app.route("/")
