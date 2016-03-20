fdc5ca012e4305563d2ee52f283807670356f4d5
Yoonseop Kang <e0engoon@gmail.com>
2016-03-20 17:29:24 +0900

    Automatically map objects using reflection

--- database.py
+++ database.py
@@ -11,8 +11,3 @@ db_session = scoped_session(sessionmaker(autocommit=False,
                                          bind=engine))
 Base = declarative_base()
 Base.query = db_session.query_property()
-
-
-def init_db():
-    import models
-    Base.metadata.create_all(bind=engine)
--- models.py
+++ models.py
@@ -1,55 +1,27 @@
 from datetime import datetime
 from sqlalchemy.orm import relationship, backref
-from sqlalchemy import Table, Column, Integer, String, DateTime, Text,\
-    ForeignKey
+from sqlalchemy import MetaData
+from sqlalchemy.ext.automap import automap_base
 
-from database import Base
-
-
-history_authors = Table(
-    'wiki_history_author', Base.metadata,
-    Column('author_id', Integer, ForeignKey('wiki_authors.id')),
-    Column('history_id', Integer))
-    )
+from database import engine
 
 
-class Author(Base):
-    __tablename__ = 'wiki_author'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(30))
-    password = Column(String(16))
-    email = Column(String(100))
+table_names = [
+    'wiki_author',
+    'wiki_article',
+    'wiki_media',
+    'wiki_history',
+    'wiki_history_author'
+]
+
+metadata = MetaData()
+metadata.reflect(engine, only=table_names)
+Base = automap_base(metadata=metadata)
+Base.prepare()
 
 
-class Article(Base):
-    __tablename__ = 'wiki_article'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(100))
-    ip_address = Column(String(32))
-    content = Column(Text)
-    markdown = Column(Text)
-    content_html = Column(Text)
-    time_create = Column(DateTime, default=datetime.now)
-    time_edit = Column(DateTime, default=datetime.now,
-            onupdate=datetime.now)
-    links = Column(Text, default='')
-
-
-class Media(Base):
-    __tablename__ = 'wiki_media'
-    id = Column(Integer, primary_key=True)
-    filename = Column(String(256))
-    time_create = Column(DateTime, default=datetime.now,
-            onupdate=datetime.now)
-
-
-class History(Base):
-    __tablename__ = 'wiki_history'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(100))
-    ip_address = Column(String(32))
-
-    author = relationship("Author", secondary=history_authors)
-    content = Column(Text, default='')
-    time = Column(DateTime, onupdate=datetime.now)
-    type = Column(String(16), default='mod')
+Author = Base.classes.wiki_author
+Article = Base.classes.wiki_article
+Media = Base.classes.wiki_media
+History = Base.classes.wiki_history
+HistoryAuthor = Base.classes.wiki_history_author
