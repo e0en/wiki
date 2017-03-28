0400b071c0ac134dbf63fdc793d3de2d37703de9
Yoonseop Kang <e0engoon@gmail.com>
2017-03-28 22:18:01 +0900

    Remove redundant flask.g references

--- model.py
+++ model.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import sqlite3
+from flask import g
 import secret
 
 
-def get_db(g):
+def get_db():
     db = getattr(g, '_database', None)
     if db is None:
         db = g._database = sqlite3.connect(secret.DATABASE)
@@ -12,27 +13,27 @@ def get_db(g):
     return db
 
 
-def query_db(query, g, args=(), one=False):
-    cur = get_db(g).execute(query, args)
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
     rv = cur.fetchall()
     cur.close()
     return (rv[0] if rv else None) if one else rv
 
 
 class Article(object):
-    def __init__(self, g):
-        self.g = g
+    def __init__(self):
+        pass
 
     def filter_by(self, pagename):
         query_str = "SELECT * FROM wiki_article WHERE name='%s'" % pagename
-        return query_db(query_str, self.g, one=True)
+        return query_db(query_str, one=True)
 
     def prev(self, pagename):
         prev_query = "SELECT name FROM wiki_article " + \
             "WHERE name<'%s' ORDER BY name desc LIMIT 1" % pagename
-        return query_db(prev_query, self.g, one=True)
+        return query_db(prev_query, one=True)
 
     def next(self, pagename):
         next_query = "SELECT name FROM wiki_article " + \
             "WHERE name>'%s' ORDER BY name LIMIT 1" % pagename
-        return query_db(next_query, self.g, one=True)
+        return query_db(next_query, one=True)
