b12004dc0642b380725cba75a38199f9ac2a0acc
Yoonseop Kang <e0engoon@gmail.com>
2019-07-28 07:47:54 +0000

    Fix byte counter in edit page

--- static/edit.js
+++ static/edit.js
@@ -1,10 +1,14 @@
 window.onload = function() {
-    var content = document.getElementById("content");
-    var count = document.getElementById("count");
+  var content = document.getElementById("content");
+  var count = document.getElementById("count");
 
-    var display_char_count = function() {
-        var n_char = content.value.length;
-	count.innerHTML = String(n_char);
-    };
+  var display_char_count = function() {
+    var n_char = content.value.length;
+    count.innerHTML = String(n_char);
+  };
+  display_char_count();
+
+  content.addEventListener("keyup", event => {
     display_char_count();
+  });
 };
