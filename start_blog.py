d8c0ba0f9c354a78fb3f06b1fa994ae16ce580c6
Yoonseop Kang <e0engoon@gmail.com>
2019-11-02 09:27:22 +0000

    Replace gevent start script w/ gunicorn

--- start_blog.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-from gevent.pywsgi import WSGIServer
-from blog import app
-
-
-http_server = WSGIServer(('', 5001), app)
-http_server.serve_forever()
