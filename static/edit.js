28e543c7c504137337e632ba57dd531740234023
Yoonseop Kang <e0engoon@gmail.com>
2019-12-14 09:23:11 +0000

    Make mobile edit field full-screen

--- static/edit.js
+++ static/edit.js
@@ -1,15 +1,9 @@
 window.onload = function() {
-  const content = document.getElementById('content');
   var count = document.getElementById('count');
 
   function update_content_field() {
     const n_char = content.value.length
     count.innerHTML = String(n_char)
-    content.style.height = ''
-    const height = content.scrollHeight;
-    content.style.height = height + 'px';
-    content.style.maxHeight = height + 'px';
-
   };
   update_content_field();
 
