807de92386c615ed0ee070748a8f4ad4f87b42f6
Yoonseop Kang <e0engoon@gmail.com>
2019-12-15 03:49:42 +0000

    Give focus to textarea on edit page load

--- static/edit.js
+++ static/edit.js
@@ -1,13 +1,16 @@
 window.onload = function() {
-  var count = document.getElementById('count');
+  const count = document.getElementById('count')
+  const content = document.getElementById('content')
 
   function update_content_field() {
     const n_char = content.value.length
     count.innerHTML = String(n_char)
-  };
-  update_content_field();
+  }
+
+  content.focus()
+  update_content_field()
 
   content.addEventListener('input', event => {
-    update_content_field();
-  });
+    update_content_field()
+  })
 };
